aeced6954e27bed0ce26fd31042af0bd5c450118
Guy Bianco IV <gbianco@redhat.com>
2017-09-07 19:33:34 -0400

    add files and update README

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # mvr
 mvr - minimum viable reddit
+
+## About
+
+mvr strives to be the minimum extra features you need to make browsing reddit comfortable. It takes inspiration from Reddit Enhancement Suite (RES) and came about due to the latter dropping support for Safari. It does not (and will not) attempt to recreate the vast majority of RES.
+
+mvr mainly focuses on adding basic navigation keyboard controls (see below). Speed is another focus.
+
+## Support
+
+mvr consists of a small JavaScript file. This allows it to be snappy and work cross-browser. 
+
+Currently, there is only a build for Safari, but Chrome and Firefox should be coming next.
+
+## Controls
+
+j - select next post
+k - select previous post
+h - toggle in-line viewing ("expando")
+l - open post in a new tab
+
+m - open comments in a new tab
+. - go to next page
+, - go to previous page
---
+++ assets/main.js
@@ -0,0 +1,110 @@
+/*
+ * MVR - Minimum Viable Reddit
+ * 
+ * author:  Guy Bianco IV <@gjbiancoiv>
+ * created: September 7, 2017
+ */
+(function() {
+  'use strict';
+
+  // constants
+  var SELECTED_CLASS = 'selectedPost';
+  var SCROLL_OFFSET = -5; // pixels
+
+  // good ol' global variables :P
+  var posts;
+  var currentPost = 0;
+  var expanded = false;
+
+  function init() {
+
+    // grab our list of posts
+    var postList = document.getElementById('siteTable');
+    posts = postList.querySelectorAll('.thing');
+
+    // select default intial post
+    posts[currentPost].classList.add(SELECTED_CLASS);
+
+    // TODO
+    // select post when clicked
+    // for(var pi in posts) {
+    //   var p = posts[pi];
+    //   p.addEventListener('click', function() {
+    //     changeSelected(0);
+    //   });
+    // }
+
+    document.addEventListener('keydown', function(key) {
+      switch(key.code) {
+        case 'KeyJ':
+          // select next post
+          changeSelected(currentPost + 1);
+          break;
+        case 'KeyK':
+          // select prev post
+          changeSelected(currentPost - 1);
+          break;
+        case 'KeyH':
+          // toggles in-line viewing ("expando")
+          expanded = !expanded;
+          toggleSingleExpando(posts[currentPost]);
+          break;
+        case 'KeyL':
+          // open post in new tab
+          window.open(posts[currentPost].querySelector('a.title').href);
+          break;
+        case 'KeyM':
+          // open comments in new tab
+          window.open(posts[currentPost].querySelector('.first>a').href);
+          break;
+        case 'Period':
+          // go to next page
+          window.location = document.querySelector('.next-button > a').href;
+          break;
+        case 'Comma':
+          // go to prev page
+          window.location = document.querySelector('.prev-button > a').href;
+          break
+      }
+    });
+  }
+
+  // handles updating DOM when our selected post changes
+  function changeSelected(newIndex) {
+    // change our current post
+    var oldPost = posts[currentPost];
+    currentPost = Math.max(0, Math.min(newIndex, posts.length - 1));
+    var newPost = posts[currentPost];
+
+    // change our post
+    oldPost.classList.remove(SELECTED_CLASS);
+    newPost.classList.add(SELECTED_CLASS);
+    if(expanded) {
+      toggleSingleExpando(oldPost);
+      toggleSingleExpando(newPost);
+    }
+
+    // scroll to post
+    // FIX has issue scrolling to last post on a page (possibly others?)
+    window.scroll(0, [newPost.offsetTop + newPost.offsetParent.offsetTop + SCROLL_OFFSET]);
+  }
+
+  function toggleSingleExpando(post) {
+    var expando = post.querySelector('.expando-button');
+    if(expando) {
+      expando.click();
+    }
+  }
+
+  // initialize on page load
+  if(window.onload) {
+    var curronload = window.onload;
+    var newonload = function(evt) {
+      curronload(evt);
+      init(evt);
+    };
+    window.onload = newonload;
+  } else {
+    window.onload = init;
+  }
+})();
